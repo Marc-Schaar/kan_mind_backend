eb674a6602fbcd8fefe27d949126cf3476ca814b
Marc Schaar <m-schaar@gmx.net>
2025-07-01 19:11:56 +0200

    refactor: pip8 to kanban_app

--- kanban_app/api/serializer.py
+++ kanban_app/api/serializer.py
@@ -1,6 +1,6 @@
+from django.contrib.auth.models import User
 from rest_framework import serializers
 from kanban_app.models import Boards, Task, Comment
-from django.contrib.auth.models import User
 
 
 class UserSerializer(serializers.ModelSerializer):
